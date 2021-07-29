b818c67d3fa28326dd256bcea13ad7957c99707a
Zhaohe <Z.He@dal.ca>
2021-07-29 17:09:29 -0300

    ...

--- js/mp3Upload.js
+++ js/mp3Upload.js
@@ -1,20 +1,14 @@
-function callback(file) {
-  console.log("here");
-  console.log(file.files);
-  console.log(file);
-}
-
 function uploadClicked() {
   let file = document.getElementsByName("fileInput")[0].files[0];
   console.log(file);
+
   computeMD5(file, tryHashUpload)
 };
 
 function contQuery(id) {
-  console.log("enter contQuery");
-  var uploadWindow = document.getElementById("uploadCard");
+  let uploadWindow = document.getElementById("uploadCard");
   uploadWindow.style.display = "none";
-  var wipWindow = document.getElementById("wipCard");
+  let wipWindow = document.getElementById("wipCard");
   wipWindow.style.display = "";
   polling(id)
 };
@@ -35,7 +29,7 @@ function polling(id){
       }else{
         console.log("download URL: "+data);
         document.getElementById("wipCard").style.display = "none";
-        var resultWindow = document.getElementById("resultCard");
+        let resultWindow = document.getElementById("resultCard");
         resultWindow.style.display = "";
         document.getElementById("downloadBtn").setAttribute("href",data);
       }
@@ -56,23 +50,27 @@ function tryHashUploadCallback(response) {
   console.log(response)
 };
 function tryHashUpload(hash) {
+  let begin = document.getElementById("beginInput").value;
+  let end = document.getElementById("endInput").value;
   axios.request({
     method: "get",
     url: "/Splitter_war_exploded/HashUploadServlet",
     params: {
       userToken: 1,
-      begin: 0,
-      end: 10000,
+      begin: begin,
+      end: end,
       hash: String(hash)
     }
   }
   ).then(tryHashUploadCallback)
 };
 function fileUpload(file) {
+  let begin = document.getElementById("beginInput").value;
+  let end = document.getElementById("endInput").value;
   let params = new FormData()
   params.append('userToken', userToken)
-  params.append("begin", "0")
-  params.append("end", "1000")
+  params.append("begin", begin)
+  params.append("end", end)
   params.append('mp3', file)
   axios({
     method: 'post',
@@ -96,7 +94,7 @@ function fileUpload(file) {
   })
 };
 function computeMD5(f, callback) {
-  var blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice,
+  let blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice,
     file = f,
     chunkSize = 2097152,                             // Read in chunks of 2MB
     chunks = Math.ceil(file.size / chunkSize),
