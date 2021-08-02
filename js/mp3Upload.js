3e2d6b1702dd20c4419cf161c2bc966eb3b7d08a
Zhe Ye <61473610+alb876237266@users.noreply.github.com>
2021-08-02 15:09:44 -0300

    Update mp3Upload.js

--- js/mp3Upload.js
+++ js/mp3Upload.js
@@ -21,25 +21,25 @@ function contQuery(id) {
 
 function polling(id){
   axios.request(
-    {
-      method: "get",
-      url: "/Splitter_war_exploded/GetTaskResultServlet?taskID="+String(id)
-    }
-  ).then(
-    function (response) {
-      console.log("polling response: ");
-      console.log(response);
-      data=response.data;
-      if(data==="no result"){
-        setTimeout(function(){polling(id)},3000)
-      }else{
-        console.log("download URL: "+data);
-        document.getElementById("wipCard").style.display = "none";
-        var resultWindow = document.getElementById("resultCard");
-        resultWindow.style.display = "";
-        document.getElementById("downloadBtn").setAttribute("href",data);
+      {
+        method: "get",
+        url: "/Splitter_war_exploded/GetTaskResultServlet?taskID="+String(id)
       }
-    }
+  ).then(
+      function (response) {
+        console.log("polling response: ");
+        console.log(response);
+        data=response.data;
+        if(data==="no result"){
+          setTimeout(function(){polling(id)},3000)
+        }else{
+          console.log("download URL: "+data);
+          document.getElementById("wipCard").style.display = "none";
+          var resultWindow = document.getElementById("resultCard");
+          resultWindow.style.display = "";
+          document.getElementById("downloadBtn").setAttribute("href",data);
+        }
+      }
   ).catch(function (error) {
     console.log(error)
   })
@@ -56,34 +56,38 @@ function tryHashUploadCallback(response) {
   console.log(response)
 };
 function tryHashUpload(hash) {
+    let begin = document.getElementById("beginInput").value;
+    let end = document.getElementById("endInput").value;
   axios.request({
-    method: "get",
-    url: "/Splitter_war_exploded/HashUploadServlet",
-    params: {
-      userToken: 1,
-      begin: 0,
-      end: 10000,
-      hash: String(hash)
-    }
-  }
+        method: "get",
+        url: "/Splitter_war_exploded/HashUploadServlet",
+        params: {
+          userToken: 1,
+          begin: begin,
+          end: end,
+          hash: String(hash)
+        }
+      }
   ).then(tryHashUploadCallback)
 };
 function fileUpload(file) {
+    let begin = document.getElementById("beginInput").value;
+    let end = document.getElementById("endInput").value;
   let params = new FormData()
   params.append('userToken', userToken)
-  params.append("begin", "0")
-  params.append("end", "1000")
+  params.append("begin", begin)
+  params.append("end", end)
   params.append('mp3', file)
   axios({
-    method: 'post',
-    url: '/Splitter_war_exploded/FileUploadServlet',
-    data: params,
-    headers: { 'Content-Type': 'multipart/form-data;boundary = ' + new Date().getTime() },
-    onUploadProgress: function (p) {
-      let complete = (p.loaded / p.total * 100 | 0) + '%'
-      console.log('上传' + complete)
-    }
-  }
+        method: 'post',
+        url: '/Splitter_war_exploded/FileUploadServlet',
+        data: params,
+        headers: { 'Content-Type': 'multipart/form-data;boundary = ' + new Date().getTime() },
+        onUploadProgress: function (p) {
+          let complete = (p.loaded / p.total * 100 | 0) + '%'
+          console.log('上传' + complete)
+        }
+      }
   ).then(function (response) {
     if (response.data["state"] === "failed") {
       console.log("file upload failed");
@@ -97,12 +101,12 @@ function fileUpload(file) {
 };
 function computeMD5(f, callback) {
   var blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice,
-    file = f,
-    chunkSize = 2097152,                             // Read in chunks of 2MB
-    chunks = Math.ceil(file.size / chunkSize),
-    currentChunk = 0,
-    spark = new SparkMD5.ArrayBuffer(),
-    fileReader = new FileReader();
+      file = f,
+      chunkSize = 2097152,                             // Read in chunks of 2MB
+      chunks = Math.ceil(file.size / chunkSize),
+      currentChunk = 0,
+      spark = new SparkMD5.ArrayBuffer(),
+      fileReader = new FileReader();
   fileReader.onload = function (e) {
     console.log('read chunk nr', currentChunk + 1, 'of', chunks);
     spark.append(e.target.result);                   // Append array buffer
@@ -121,7 +125,7 @@ function computeMD5(f, callback) {
   };
   function loadNext() {
     var start = currentChunk * chunkSize,
-      end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
+        end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
     fileReader.readAsArrayBuffer(blobSlice.call(file, start, end));
   }
   loadNext();
